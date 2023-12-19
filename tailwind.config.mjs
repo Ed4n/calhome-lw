8c0a8e482e6e11c6c0e2079a868161410a0af5eb
Ed4n <edgardo.avalencia@gmail.com>
2023-12-19 16:32:42 -0600

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				"lw-blue": "#00117A",
+				"lw-blue-2": "#486BD9",
+				"lw-blue-3": "#027FF9",
+				"lw-orange-1": "#FCA311",
+				"lw-orange-2": "#FFC30F",
+				"lw-orange-3": "#F5DF80",
+			}
+		},
+	},
+	plugins: [],
+}
